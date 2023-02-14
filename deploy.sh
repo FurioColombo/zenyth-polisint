bfc2ca7546432f80c10a28921bc366595f1faf0d
Marco Furio Colombo <marco.furio.colombo@gmail.com>
2023-02-14 11:00:12 +0100

    deploy build

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:EliaPirrello/zenyth.git main:gh-pages
-
-
-cd -
+# git push -f git@github.com:FurioColombo/zenyth-polisint.git main:gh-pages
 
 git subtree push --prefix dist origin gh-pages
